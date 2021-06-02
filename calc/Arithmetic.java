9c1baac88a5836b6fd7f2b8781f7915877ae896a
Melissa Rice <mmrice22@gmail.com>
2021-06-02 15:50:50 +0000

    cubed function works

--- calc/Arithmetic.java
+++ calc/Arithmetic.java
@@ -6,23 +6,23 @@ public class Arithmetic {
 	}
 
 	public static float subtract(float num1, float num2) {
-		return 10.0f;
+		return num1 - num2;
 	}
 
 	public static float multiply(float num1, float num2) {
-		return 10.0f;
+		return num1 * num2;
 	}
 
 	public static float divide(float num1, float num2) {
-		return 10.0f;
+		return num1 / num2;
 	}
 
 	public static float square(float num1) {
-		return 10.0f;
+		return num1 * num1;
 	}
 
 	public static float cube(float num1) {
-		return 10.0f;
+		return num1 * num1 * num1;
 	}
 
 	public static float power(float num1, float num2) {
